7bddcdd926f3b5ca28caccc2f53ea53db219db88
xhochipe <95028555@qq.com>
2019-02-17 13:04:53 +0800

    路由分组

--- routers/handlers.go
+++ routers/handlers.go
@@ -1,1 +1,16 @@
 package routers
+
+import "github.com/gin-gonic/gin"
+
+func userGroupHandler(r *gin.Engine) {
+	u := r.Group("user")
+	{
+		u.GET("/ping", ping)
+	}
+}
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ping",
+	})
+}
--- routers/router.go
+++ routers/router.go
@@ -6,5 +6,8 @@ func InitRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+
+	//加载userGroupHandler路由
+	userGroupHandler(r)
 	return r
 }
